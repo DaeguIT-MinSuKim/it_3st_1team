add628cd211e7dd02902899955156f4291f3f96a
EunAe Kim <eunae0474@naver.com>
2018-02-21 18:24:03 +0900

    도서 신청 ui

--- src/main/java/it_3st_1team/ui/RequestBookUI.java
+++ src/main/java/it_3st_1team/ui/RequestBookUI.java
@@ -1,19 +1,134 @@
 package it_3st_1team.ui;
 
-import javax.swing.JPanel;
 import java.awt.Color;
-import javax.swing.JTable;
-import javax.swing.table.DefaultTableModel;
+import java.awt.Font;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+
+@SuppressWarnings("serial")
 public class RequestBookUI extends JPanel {
-	private JTable table;
+	private JTextField tfBookcode;
+	private JTextField tfauthor;
+	private JTextField tfpublish;
+	private JTextField tfpubyear;
+	private JTextField tfisbn;
+	private JButton btnSave;
+	private JButton btnCancel;
+	private JPanel panel;
+	private JLabel lblprice;
+	private JTextField textField;
+	private JLabel label;
 
 	public RequestBookUI() {
-		
 		initComponents();
 	}
 	private void initComponents() {
-		setBackground(new Color(0, 100, 0));
+		setBackground(new Color(224,224,224));
+		setLayout(null);
+		
+		panel = new JPanel();
+		panel.setBackground(new Color(224,224,224));
+		panel.setBounds(130, 100, 950, 400);
+		add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblBookcode = new JLabel("도서명");
+		lblBookcode.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
+		lblBookcode.setBounds(173, 94, 73, 15);
+		panel.add(lblBookcode);
+		
+		JLabel lblauthor = new JLabel("저자");
+		lblauthor.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
+		lblauthor.setBounds(173, 133, 73, 15);
+		panel.add(lblauthor);
+		
+		JLabel lblpublish = new JLabel("출판사");
+		lblpublish.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
+		lblpublish.setBounds(173, 173, 73, 15);
+		panel.add(lblpublish);
+		
+		JLabel lblpubyear = new JLabel("출판년도");
+		lblpubyear.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
+		lblpubyear.setBounds(173, 213, 73, 15);
+		panel.add(lblpubyear);
+		
+		JLabel lblIsbn = new JLabel("ISBN");
+		lblIsbn.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
+		lblIsbn.setBounds(173, 253, 73, 15);
+		panel.add(lblIsbn);
+		
+		lblprice = new JLabel("가격");
+		lblprice.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
+		lblprice.setBounds(173, 293, 73, 15);
+		panel.add(lblprice);
+		
+		tfBookcode = new JTextField();
+		tfBookcode.setBorder(new CompoundBorder(new LineBorder(new Color(192, 192, 192)), new EmptyBorder(0, 10, 0, 0)));
+		tfBookcode.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
+		tfBookcode.setBounds(258, 86, 404, 30);
+		panel.add(tfBookcode);
+		tfBookcode.setColumns(10);
+		
+		tfauthor = new JTextField();
+		tfauthor.setBorder(new CompoundBorder(new LineBorder(new Color(192, 192, 192)), new EmptyBorder(0, 10, 0, 0)));
+		tfauthor.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
+		tfauthor.setBounds(258, 126, 404, 30);
+		panel.add(tfauthor);
+		tfauthor.setColumns(10);
+		
+		tfpublish = new JTextField();
+		tfpublish.setBorder(new CompoundBorder(new LineBorder(new Color(192, 192, 192)), new EmptyBorder(0, 10, 0, 0)));
+		tfpublish.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
+		tfpublish.setBounds(258, 166, 404, 30);
+		panel.add(tfpublish);
+		tfpublish.setColumns(10);
+		
+		tfpubyear = new JTextField();
+		tfpubyear.setBorder(new CompoundBorder(new LineBorder(new Color(192, 192, 192)), new EmptyBorder(0, 10, 0, 0)));
+		tfpubyear.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
+		tfpubyear.setBounds(258, 206, 404, 30);
+		panel.add(tfpubyear);
+		tfpubyear.setColumns(10);
+		
+		tfisbn = new JTextField();
+		tfisbn.setBorder(new CompoundBorder(new LineBorder(new Color(192, 192, 192)), new EmptyBorder(0, 10, 0, 0)));
+		tfisbn.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
+		tfisbn.setBounds(258, 246, 404, 30);
+		panel.add(tfisbn);
+		tfisbn.setColumns(10);
+		
+		textField = new JTextField();
+		textField.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
+		textField.setColumns(10);
+		textField.setBorder(new CompoundBorder(new LineBorder(new Color(192, 192, 192)), new EmptyBorder(0, 10, 0, 0)));
+		textField.setBounds(258, 286, 404, 30);
+		panel.add(textField);
+		
+		btnSave = new JButton("저장");
+		btnSave.setBackground(new Color(52,152,219));
+		btnSave.setForeground(Color.WHITE);
+		btnSave.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
+		btnSave.setBounds(592, 326, 70, 30);
+		panel.add(btnSave);
+		
+		btnCancel = new JButton("취소");
+		btnCancel.setForeground(Color.WHITE);
+		btnCancel.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
+		btnCancel.setBackground(new Color(128, 128, 128));
+		btnCancel.setBounds(499, 326, 70, 30);
+		panel.add(btnCancel);
+		
+		label = new JLabel("희망 도서 입력");
+		label.setFont(new Font("맑은 고딕", Font.BOLD, 36));
+		label.setBounds(173, 21, 360, 53);
+		panel.add(label);
 	}
 
 }
--- src/main/java/it_3st_1team/ui/UserUI.java
+++ src/main/java/it_3st_1team/ui/UserUI.java
@@ -11,6 +11,10 @@ import java.awt.event.ActionEvent;
 public class UserUI extends JPanel implements ActionListener {
 	private JButton btnBookSearch;
 	private JPanel show;
+	private JButton btnBookRequest;
+	private JButton btnHistory;
+	private JButton btnNews;
+	private JButton btnUserUpdate;
 	
 	public UserUI() {
 		initComponents();
@@ -32,28 +36,29 @@ public class UserUI extends JPanel implements ActionListener {
 		btnBookSearch.setBounds(0, 50, 250, 45);
 		panel.add(btnBookSearch);
 		
-		JButton btnBookRequest = new JButton("도서 신청");
+		btnBookRequest = new JButton("도서 신청");
+		btnBookRequest.addActionListener(this);
 		btnBookRequest.setFont(new Font("맑은 고딕", Font.BOLD, 18));
 		btnBookRequest.setForeground(new Color(255, 255, 255));
 		btnBookRequest.setBackground(new Color(158,158,158));
 		btnBookRequest.setBounds(0, 110, 250, 45);
 		panel.add(btnBookRequest);
 		
-		JButton btnHistory = new JButton("대출/반납 이력");
+		btnHistory = new JButton("대출/반납 이력");
 		btnHistory.setFont(new Font("맑은 고딕", Font.BOLD, 18));
 		btnHistory.setForeground(new Color(255, 255, 255));
 		btnHistory.setBackground(new Color(158,158,158));
 		btnHistory.setBounds(0, 170, 250, 45);
 		panel.add(btnHistory);
 		
-		JButton btnNews = new JButton("공지 사항");
+		btnNews = new JButton("공지 사항");
 		btnNews.setFont(new Font("맑은 고딕", Font.BOLD, 18));
 		btnNews.setForeground(new Color(255, 255, 255));
 		btnNews.setBackground(new Color(158,158,158));
 		btnNews.setBounds(0, 230, 250, 45);
 		panel.add(btnNews);
 		
-		JButton btnUserUpdate = new JButton("개인 정보 수정");
+		btnUserUpdate = new JButton("개인 정보 수정");
 		btnUserUpdate.setFont(new Font("맑은 고딕", Font.BOLD, 18));
 		btnUserUpdate.setForeground(new Color(255, 255, 255));
 		btnUserUpdate.setBackground(new Color(158,158,158));
@@ -72,15 +77,35 @@ public class UserUI extends JPanel implements ActionListener {
 		add(show);
 	}
 	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnBookRequest) {
+			actionPerformedBtnBookRequest(e);
+		}
 		if (e.getSource() == btnBookSearch) {
 			actionPerformedBtnBookSearch(e);
 		}
 	}
 	protected void actionPerformedBtnBookSearch(ActionEvent e) {
+		show.removeAll();
 		btnBookSearch.setBackground(new Color(127,127,127));
+		btnBookRequest.setBackground(new Color(158,158,158));
+		btnHistory.setBackground(new Color(158,158,158));
+		btnNews.setBackground(new Color(158,158,158));
+		btnUserUpdate.setBackground(new Color(158,158,158));
 		show.repaint();
 		SearchBookUI search = new SearchBookUI();
 		search.setBounds(0, 0, 1150, 800);
 		show.add(search);
+	}
+	protected void actionPerformedBtnBookRequest(ActionEvent e) {
+		show.removeAll();
+		btnBookRequest.setBackground(new Color(127,127,127));
+		btnBookSearch.setBackground(new Color(158,158,158));
+		btnHistory.setBackground(new Color(158,158,158));
+		btnNews.setBackground(new Color(158,158,158));
+		btnUserUpdate.setBackground(new Color(158,158,158));
+		show.repaint();
+		RequestBookUI request = new RequestBookUI();
+		request.setBounds(0, 0, 1150, 800);
+		show.add(request);
 	}
 }
