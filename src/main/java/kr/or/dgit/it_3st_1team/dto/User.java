783ba782603579459bd86df15a41d007ef215072
hanseok Park <es12850@naver.com>
2018-03-05 09:19:10 +0900

    ..

--- src/main/java/kr/or/dgit/it_3st_1team/dto/User.java
+++ src/main/java/kr/or/dgit/it_3st_1team/dto/User.java
@@ -3,13 +3,15 @@ package kr.or.dgit.it_3st_1team.dto;
 import java.util.Date;
 
 public class User {
-	private int code;
+	private String code;
 	private String name;
 	private String id;
 	private String pw;
 	private Phone tel;
 	private String email;
+	private Post zipcode;
 	private Post addr_id;
+	private String addr_de;
 	private boolean blacktf;
 	private Date entryday;
 	private Date freeday;
@@ -17,10 +19,61 @@ public class User {
 	public User() {
 		// TODO Auto-generated constructor stub
 	}
-	public int getCode() {
+	
+	public User(String code) {
+		super();
+		this.code = code;
+	}
+	
+	public User(String name, String pw, Phone tel, String email, Post addr_id) {
+		super();
+		this.name = name;
+		this.pw = pw;
+		this.tel = tel;
+		this.email = email;
+		this.addr_id = addr_id;
+	}
+
+
+	public User(String code, String name, String id, String pw, Phone tel, String email, Post addr_id, Date entryday) {
+		super();
+		this.code = code;
+		this.name = name;
+		this.id = id;
+		this.pw = pw;
+		this.tel = tel;
+		this.email = email;
+		this.addr_id = addr_id;
+		this.entryday = entryday;
+	}
+	
+	public User(String code, String name, String id, String pw, Phone tel, String email, Post addr_id, String addr_de,
+			boolean blacktf, Date entryday, Date freeday) {
+		super();
+		this.code = code;
+		this.name = name;
+		this.id = id;
+		this.pw = pw;
+		this.tel = tel;
+		this.email = email;
+		this.addr_id = addr_id;
+		this.addr_de = addr_de;
+		this.blacktf = blacktf;
+		this.entryday = entryday;
+		this.freeday = freeday;
+	}
+
+
+	public String getAddr_de() {
+		return addr_de;
+	}
+	public void setAddr_de(String addr_de) {
+		this.addr_de = addr_de;
+	}
+	public String getCode() {
 		return code;
 	}
-	public void setCode(int code) {
+	public void setCode(String code) {
 		this.code = code;
 	}
 	public String getName() {
@@ -77,14 +130,17 @@ public class User {
 	public void setFreeday(Date freeday) {
 		this.freeday = freeday;
 	}
+	public synchronized Post getZipcode() {
+		return zipcode;
+	}
+	public synchronized void setZipcode(Post zipcode) {
+		this.zipcode = zipcode;
+	}
+
 	@Override
 	public String toString() {
 		return String.format(
-				"User [code=%s, name=%s, id=%s, pw=%s, tel=%s, email=%s, addr_id=%s, blacktf=%s, entryday=%s, freeday=%s]",
-				code, name, id, pw, tel, email, addr_id, blacktf, entryday, freeday);
+				"User [code=%s, name=%s, id=%s, pw=%s, tel=%s, email=%s, zipcode=%s, addr_id=%s, addr_de=%s, blacktf=%s, entryday=%s, freeday=%s]",
+				code, name, id, pw, tel, email, zipcode, addr_id, addr_de, blacktf, entryday, freeday);
 	}
-	
-	
-	
-	
 }
